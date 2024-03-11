098379d942a17b6a754e99a5fb9081d4a7b579d1
Олег <olegyarch@gmail.com>
2024-03-11 22:33:48 +0200

    Завдання №2

---
+++ file2.py
@@ -0,0 +1,6 @@
+user_input = input("Введіть 4-х значне число: ")
+
+print(int(user_input) // 1000)
+print(int(user_input) // 100 % 10)
+print(int(user_input) % 100 // 10)
+print(int(user_input) % 100 % 10)
