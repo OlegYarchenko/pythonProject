345c9651ce83d8cd3dfce4c0545c40d6798c9e32
Олег <olegyarch@gmail.com>
2024-03-31 13:46:49 +0300

    Завдання №3.2

---
+++ file3_2.py
@@ -0,0 +1,9 @@
+list_one = [12, 3, 4, 10]
+list_two = [1]
+list_three = []
+list_four = [12, 3, 4, 10, 8]
+
+print(list_one[-1:]+list_one[0:-1])
+print(list_two[-1:]+list_two[0:-1])
+print(list_three[-1:]+list_three[0:-1])
+print(list_four[-1:]+list_four[0:-1])
