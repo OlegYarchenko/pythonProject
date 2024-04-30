cde9a1ce330b75ebce3d1df1675bd44f857512aa
Олег <olegyarch@gmail.com>
2024-04-30 10:19:28 +0300

    Завдання №4.1

---
+++ file4_1.py
@@ -0,0 +1,13 @@
+my_list = [0, 1, 0, 12, 3]
+# my_list = [0]
+# my_list = [1, 0, 13, 0, 0, 0, 5]
+# my_list = [9, 0, 7, 31, 0, 45, 0, 45, 0, 45, 0, 0, 96, 0]
+
+print(my_list)
+list_len = len(my_list) + 1
+
+for list_index in my_list:
+    if list_index == 0:
+        my_list.insert(list_len, my_list.pop(my_list.index(list_index)))
+
+print(my_list)
