c43885ab9ebcf82a54f30915c2cf2d3b498251c3
Олег <olegyarch@gmail.com>
2024-05-01 11:00:50 +0300

    Завдання №4.2

--- file4_2.py
+++ file4_2.py
@@ -10,6 +10,8 @@ else:
     my_second_list = my_list[0:list_len:2]
     last_list_element = my_list.pop()
     sum_element = 0
+
     for list_element in my_second_list:
         sum_element += list_element
+
     print(sum_element * last_list_element)
