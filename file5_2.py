1dc75ebae1cc9e90f7f0a8c08b4ab8717fe45aeb
Олег <olegyarch@gmail.com>
2024-07-28 16:53:21 +0300

    Завдання №5.2

--- file5_2.py
+++ file5_2.py
@@ -1,23 +1,26 @@
+my_dija_list = ["+", "-", "*", "/"]
+
 while True:
     my_status_program = input("Запустити калькулятор? (y/n): " )
     if my_status_program == "y":
         input_chislo1 = input("Введіть перше число: ")
         input_chislo2 = input("Введіть друге число: ")
         input_dija = input("Яку дію слід виконати (+,-,*,/)?: ")
-        if input_dija == "+":
-            print(int(input_chislo1) + int(input_chislo2))
-        elif input_dija == "-":
-            print(int(input_chislo1) - int(input_chislo2))
-        elif input_dija == "*":
-            print(int(input_chislo1) * int(input_chislo2))
-        elif input_dija == "/":
+        if input_dija == "+" and input_chislo1.isdigit() and input_chislo2.isdigit() and input_dija in my_dija_list:
+            my_result = int(input_chislo1) + int(input_chislo2)
+        elif input_dija == "-" and input_chislo1.isdigit() and input_chislo2.isdigit() and input_dija in my_dija_list:
+            my_result = int(input_chislo1) - int(input_chislo2)
+        elif input_dija == "*" and input_chislo1.isdigit() and input_chislo2.isdigit() and input_dija in my_dija_list:
+            my_result = int(input_chislo1) * int(input_chislo2)
+        elif input_dija == "/" and input_chislo1.isdigit() and input_chislo2.isdigit() and input_dija in my_dija_list:
             if int(input_chislo2) == 0:
-                print("На нуль ділити не можна!")
+                my_result = "На нуль ділити не можна!"
             else:
-                print(int(input_chislo1) / int(input_chislo2))
+                my_result = int(input_chislo1) / int(input_chislo2)
         else:
-            print("Ви щось не те ввели(")
+            my_result = "Ви щось не те ввели("
     elif my_status_program == "n":
         break
     else:
-        print("Ви щось не так ввели")
+        my_result = "Ви щось не так ввели"
+    print(my_result)
